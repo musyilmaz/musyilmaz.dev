06d421520976d40b46d4bcfbc01183956a47c54b
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2022-11-03 15:26:57 +0300

    Add brand colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      brand: {
+        100: "#E0E1DD",
+        200: "#778DA9",
+        300: "#415A77",
+        400: "#1B263B",
+        500: "0D1B2A",
+      },
+    },
     extend: {
       fontFamily: {
         rubik: ["Rubik", "sans-serif"],
